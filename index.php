0bc6c9fcdb7a099ed82877685cefc82be44eca75
Matyas Danter <mdanter@gmail.com>
2016-11-16 05:05:24 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,34 @@
+<h2>SERVER</h2>
 <pre>
 <?php
-
+print_r($_SERVER);
+?>
+</pre>
+<hr />
+<h2>COOKIE</h2>
+<pre>
+<?php
 print_r($_COOKIE);
-
+?>
+</pre>
+<hr />
+<h2>REQUEST</h2>
+<pre>
+<?php
+print_r($_REQUEST);
+?>
+</pre>
+<hr />
+<h2>SESSION</h2>
+<pre>
+<?php
+print_r($_SESSION);
+?>
+</pre>
+<hr />
+<h2>ENV</h2>
+<pre>
+<?php
+print_r($_ENV);
 ?>
 </pre>
